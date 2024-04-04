0363f72ffdc0efe653cc961a3698e72f4c14c7aa
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-04-04 11:03:58 +0530

    induction heads in GPT2 using hooks

---
+++ mechanistic-interpretability-introduction/utils.py
@@ -0,0 +1,34 @@
+from transformer_lens import utils, HookedTransformer, HookedTransformerConfig, FactoredMatrix, ActivationCache
+from jaxtyping import Int, Float
+from torch import Tensor
+import torch as t
+from typing import List, Optional, Tuple
+
+def generate_repeated_tokens(
+    model: HookedTransformer, seq_len: int, batch: int = 1
+) -> Int[Tensor, "batch full_seq_len"]:
+    '''
+    Generates a sequence of repeated random tokens
+
+    Outputs are:
+        rep_tokens: [batch, 1+2*seq_len]
+    '''
+    prefix = (t.ones(batch, 1) * model.tokenizer.bos_token_id).long()
+    first_half = t.randint(0, model.cfg.d_vocab, (batch, seq_len))
+    rep_tokens = t.concat([prefix, first_half, first_half], dim=1)
+    return rep_tokens
+
+def run_and_cache_model_repeated_tokens(model: HookedTransformer, seq_len: int, batch: int = 1) -> Tuple[t.Tensor, t.Tensor, ActivationCache]:
+    '''
+    Generates a sequence of repeated random tokens, and runs the model on it, returning logits, tokens and cache
+
+    Should use the `generate_repeated_tokens` function above
+
+    Outputs are:
+        rep_tokens: [batch, 1+2*seq_len]
+        rep_logits: [batch, 1+2*seq_len, d_vocab]
+        rep_cache: The cache of the model run on rep_tokens
+    '''
+    rep_tokens = generate_repeated_tokens(model, seq_len, batch)
+    rep_logits, rep_cache = model.run_with_cache(rep_tokens)
+    return rep_tokens, rep_logits, rep_cache
